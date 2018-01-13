fb1ebb9fb318d0360499e8df20b56e88f5431062
lihaijun <haijun.li@integrity.com.cn>
2018-01-13 14:46:58 +0800

    优化通用实体对象

--- hrhg-commons/src/main/java/com/integrity/framework/model/BaseEntity.java
+++ hrhg-commons/src/main/java/com/integrity/framework/model/BaseEntity.java
@@ -3,6 +3,8 @@
  */
 package com.integrity.framework.model;
 
+import lombok.Data;
+
 import java.util.Date;
 
 /**
@@ -11,115 +13,26 @@ import java.util.Date;
  * @author 李海军
  * @since 1.0.0
  */
+@Data
 public class BaseEntity {
     /**
      * 删除标记
      */
-    private Boolean deleteFlag;
+    protected Boolean deleteFlag;
     /**
      * 创建者
      */
-    private String creator;
+    protected String creator;
     /**
      * 创建时间
      */
-    private Date createTime;
+    protected Date createTime;
     /**
      * 更新者
      */
-    private String updator;
+    protected String updator;
     /**
      * 更新时间
      */
-    private Date updateTime;
-
-    /**
-     * 获取删除标记。<br>
-     *
-     * @return 删除标记
-     */
-    public Boolean getDeleteFlag() {
-        return deleteFlag;
-    }
-
-    /**
-     * 设置删除标记。<br>
-     *
-     * @param deleteFlag 删除标记
-     */
-    public void setDeleteFlag(Boolean deleteFlag) {
-        this.deleteFlag = deleteFlag;
-    }
-
-    /**
-     * 获取创建者。<br>
-     *
-     * @return 创建者
-     */
-    public String getCreator() {
-        return creator;
-    }
-
-    /**
-     * 设置创建者。<br>
-     *
-     * @param creator 创建者
-     */
-    public void setCreator(String creator) {
-        this.creator = creator == null ? null : creator.trim();
-    }
-
-    /**
-     * 获取创建时间。<br>
-     *
-     * @return 创建时间
-     */
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    /**
-     * 设置创建时间。<br>
-     *
-     * @param createTime 创建时间
-     */
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    /**
-     * 获取更新者。<br>
-     *
-     * @return 更新者
-     */
-    public String getUpdator() {
-        return updator;
-    }
-
-    /**
-     * 设置更新者。<br>
-     *
-     * @param updator 更新者
-     */
-    public void setUpdator(String updator) {
-        this.updator = updator == null ? null : updator.trim();
-    }
-
-    /**
-     * 获取更新时间。<br>
-     *
-     * @return 更新时间
-     */
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    /**
-     *  设置更新时间。<br>
-     *
-     * @param updateTime 更新时间
-     */
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
+    protected Date updateTime;
 }
